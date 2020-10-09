89096a4229fc3c508f84b8c6e778decb6a8f64d0
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-10-09 20:57:18 +0200

    z

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, message, args) => {
 
     try{
 
-        var text = "**HULP BOT** \n\n **__Commands__** \n .info - Geeft dit scherm. \n .new -  Maakt een ticket. \n .sollicitatie - Geeft je info over de Sollicitaties. \n .link - Hiermee heb je de invite link van de server. \n\n **__Staff Commands__** \n .training - Hiermee kan je een training inplannen. \n .ban - Hiermee kan je iemand bannen. \n .afmelden - Hiermee kan je je afmelden! \n .aanmelden - Hiermee kan je je aanmelden!"
+        var text = "**RotterdamBot** \n\n **__Commands__** \n .info - Geeft dit scherm. \n .new -  Maakt een ticket. \n .sollicitatie - Geeft je info over de Sollicitaties. \n .link - Hiermee heb je de invite link van de server. \n\n **__Staff Commands__** \n .training - Hiermee kan je een training inplannen. \n .ban - Hiermee kan je iemand bannen. \n .afmelden - Hiermee kan je je afmelden! \n .aanmelden - Hiermee kan je je aanmelden!"
         
         message.author.send(text);
 
